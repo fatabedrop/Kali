1b9cbdb27bd307a8a0610aab375bda9173abf9ec
nextia <nextia@Sebastians-MacBook-Pro.local>
2017-07-10 12:37:13 -0500

    Se insertan permisos para ubicacion en el Manifest

--- app/src/main/java/com/example/fatima/kali/MapaActivity.java
+++ app/src/main/java/com/example/fatima/kali/MapaActivity.java
@@ -1,7 +1,18 @@
 package com.example.fatima.kali;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
 
 public class MapaActivity extends AppCompatActivity {
 
@@ -10,4 +21,6 @@ public class MapaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
     }
+
+
 }
